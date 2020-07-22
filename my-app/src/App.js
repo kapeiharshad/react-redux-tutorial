a740939256e3b48f634027df479e08f26d730808
kapeiharshad <kapeiharshad@gmail.com>
2020-07-22 23:10:55 +0530

    class based component added on app.js

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,16 +2,18 @@ import React from "react";
 import Ninja from "./Ninja";
 import AddNinja from "./AddNinja";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <h1>Welcome to react</h1>
-      </header>
-      <Ninja />
-      <AddNinja />
-    </div>
-  );
+class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h1>Welcome to react</h1>
+        </header>
+        <Ninja />
+        <AddNinja />
+      </div>
+    );
+  }
 }
 
 export default App;
