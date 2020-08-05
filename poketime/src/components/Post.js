d8d2e104b9d94b6024b68d6bd7dff6ede3ec061e
kapeiharshad <kapeiharshad@gmail.com>
2020-08-06 01:55:13 +0530

    redux in progrss

--- poketime/src/components/Post.js
+++ poketime/src/components/Post.js
@@ -1,29 +1,31 @@
-import React, {Component} from "react";
-import axios from "axios";
+import React, { Component } from "react";
+import { connect } from "react-redux";
+// import axios from "axios";
 
 class Post extends Component {
-  state = {
-    id: null
-  };
-  componentDidMount() {
-    let id = this.props.match.params.post_id;
-    axios
-      .get("https://jsonplaceholder.typicode.com/posts/" + id)
-      .then((res) => {
-        this.setState({
-          post: res.data
-        });
-      });
-  }
+  // state = {
+  //   id: null
+  // };
+  // componentDidMount() {
+  //   let id = this.props.match.params.post_id;
+  //   axios
+  //     .get("https://jsonplaceholder.typicode.com/posts/" + id)
+  //     .then((res) => {
+  //       this.setState({
+  //         post: res.data
+  //       });
+  //     });
+  // }
   render() {
-    const post = this.state.post ? (
+    const post = this.props.post ? (
       <div className="post">
-        <h4 className="center">{this.state.post.title}</h4>
-        <p>{this.state.post.body}</p>
+        <h4 className="center">{this.props.post.title}</h4>
+        <p>{this.props.post.body}</p>
       </div>
     ) : (
       <div className="center">Loading post...</div>
     );
+    console.log(" this.props.post::", this.props);
     return (
       <div className="container">
         <h4>{post}</h4>
@@ -32,4 +34,14 @@ class Post extends Component {
   }
 }
 
-export default Post;
+const mapStateToProps = (state, ownProps) => {
+  let id = ownProps.match.params.post_id;
+  return {
+    post: state.posts.find((post) => {
+      return post.id === id;
+    })
+  };
+};
+console.log("mapStateToProps::", mapStateToProps);
+
+export default connect(mapStateToProps)(Post);
---
+++ poketime/src/reducers/rootReducer.js
@@ -0,0 +1,29 @@
+const initState = {
+  posts: [
+    {
+      userId: 1,
+      id: 1,
+      title:
+        "sunt aut facere repellat provident occaecati excepturi optio reprehenderit",
+      body: "quia et suscipit\nsuscipit recusandae consequuntur expedita"
+    },
+    {
+      userId: 1,
+      id: 2,
+      title: "qui est esse",
+      body: "est rerum tempore vitae\nsequi sint nihil reprehenderit"
+    },
+    {
+      userId: 1,
+      id: 3,
+      title: "ea molestias quasi exercitationem repellat qui ipsa sit aut",
+      body: "et iusto sed quo iure\nvoluptatem occaecati omnis eligendi"
+    }
+  ]
+};
+
+const rootReducer = (state = initState, action) => {
+  return state;
+};
+
+export default rootReducer;
