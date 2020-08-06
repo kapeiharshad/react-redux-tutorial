2cc6fc0181a342afa1c6617597616480a5b30d05
kapeiharshad <kapeiharshad@gmail.com>
2020-08-07 01:03:32 +0530

    map dispatch to props by redux:poketime

--- poketime/src/components/Post.js
+++ poketime/src/components/Post.js
@@ -16,11 +16,19 @@ class Post extends Component {
   //       });
   //     });
   // }
+
+  handleClick = () => {
+    this.props.deletePost(this.props.post.id);
+  };
+
   render() {
     const post = this.props.post ? (
       <div className="post">
         <h4 className="center">{this.props.post.title}</h4>
         <p>{this.props.post.body}</p>
+        <button className="btn grey" onClick={this.handleClick}>
+          Delete Post
+        </button>
       </div>
     ) : (
       <div className="center">Loading post...</div>
@@ -37,9 +45,16 @@ const mapStateToProps = (state, ownProps) => {
   let id = ownProps.match.params.post_id;
   return {
     post: state.posts.find((post) => {
-      console.log(post.id, parseInt(id));
       return post.id === parseInt(id);
     })
   };
 };
-export default connect(mapStateToProps)(Post);
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    deletePost: (id) => {
+      dispatch({ type: "DELETE_POST", id: id });
+    }
+  };
+};
+export default connect(mapStateToProps, mapDispatchToProps)(Post);
